644f102eacf3b84f6bd596add808fffac9c3ec01
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2017-09-08 17:23:03 +0200

    Added a fancy mpiport to mpi example

--- example/mpi_test.cpp
+++ example/mpi_test.cpp
@@ -11,21 +11,33 @@ struct MPIConfig {
     }
 };
 
+struct MPIPort {
+    int proc{-1};
+    int tag{-1};
+    MPIPort(int p, int t) : proc(p), tag(t) {}
+    void send(void* data, int count, MPI_Datatype type) { MPI_Send(data, count, type, proc, tag, MPI_COMM_WORLD); }
+    void receive(void* data, int count, MPI_Datatype type) {
+        MPI_Recv(data, count, type, proc, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+};
+
 struct Sender : public Component, public MPIConfig {
+    MPIPort receiver{1, 17};
     Sender() { port("hello", &Sender::hello); }
     void hello() {
         printf("<%d/%d> Hello, I am a sender!\n", rank, size);
         int number = 17;
-        MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+        receiver.send(&number, 1, MPI_INT);
     }
 };
 
 struct Receiver : public Component, public MPIConfig {
+    MPIPort sender{0, 17};
     Receiver() { port("hello", &Receiver::hello); }
     void hello() {
         printf("<%d/%d> Hello, I am a receiver!\n", rank, size);
         int number;
-        MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        sender.receive(&number, 1, MPI_INT);
         printf("<%d/%d> Received %d from process 0\n", rank, size, number);
     }
 };
