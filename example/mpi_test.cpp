95e0c1b62c5da76e5a9f8b244b33e577a91c2cdb
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2017-10-02 17:33:51 +0200

    mpi: p2p connector

--- example/mpi_test.cpp
+++ example/mpi_test.cpp
@@ -1,10 +1,15 @@
 #include <mpi.h>
 #include <stdio.h>
+#include <algorithm>
 #include <set>
 #include "tinycompo.hpp"
 
 using namespace std;
 
+/*
+===================================================================================
+  TinyCompoMPI classes
+===================================================================================*/
 struct MPIConfig {
     int rank{-1};
     int size{-1};
@@ -25,29 +30,81 @@ struct MPIPort {
     }
 };
 
-// struct Sender : public Component {
-//     MPIPort receiver{1, 17};
-//     Sender() { port("hello", &Sender::hello); }
-//     void hello() {
-//         int rank = MPIConfig().rank, size = MPIConfig().size;
-//         printf("<%d/%d> Hello, I am a sender!\n", rank, size);
-//         int number = 17;
-//         receiver.send(&number, 1, MPI_INT);
-//     }
-// };
+class MPIModel : public Model<>, public MPIConfig {
+    map<string, vector<int>> resources;
 
-// struct Receiver : public Component {
-//     MPIPort sender{0, 17};
-//     Receiver() { port("hello", &Receiver::hello); }
-//     void hello() {
-//         int rank = MPIConfig().rank, size = MPIConfig().size;
-//         printf("<%d/%d> Hello, I am a receiver!\n", rank, size);
-//         int number;
-//         sender.receive(&number, 1, MPI_INT);
-//         printf("<%d/%d> Received %d from process 0\n", rank, size, number);
-//     }
-// };
+  public:
+    void resource(const string& compo, const vector<int>& v) { resources[compo] = v; }
+    vector<int> resource(const std::string& compo) {
+        try {
+            return resources.at(compo);
+        } catch (std::out_of_range) {
+            return vector<int>();
+        }
+    }
+    bool local(const string& compo) {
+        try {
+            return find(resources.at(compo).begin(), resources.at(compo).end(), rank) != resources.at(compo).end();
+        } catch (std::out_of_range) {
+            return false;
+        }
+    }
+};
 
+Model<> emptymodel;
+
+class MPIAssembly : public Assembly<>, public MPIConfig {
+    MPIModel& internal_model;
+
+  public:
+    explicit MPIAssembly(MPIModel& model) : Assembly<>(emptymodel), internal_model(model) {
+        for (auto& c : model.components) {
+            if (model.local(c.first)) {
+                instances.emplace(c.first, std::unique_ptr<Component>(c.second._constructor()));
+            }
+        }
+        for (auto& o : model.operations) {
+            o._connect(*this);
+        }
+    }
+
+    vector<int> resource(const std::string& compo) { return internal_model.resource(compo); }
+
+    bool local(const std::string& compo) { return internal_model.local(compo); }
+};
+
+vector<int> interval(int start, int end) {
+    int aend = (end == -1) ? MPIConfig().size : end;
+    vector<int> result;
+    for (int i = start; i < aend; i++) {
+        result.push_back(i);
+    }
+    return result;
+}
+
+struct MPIp2p {
+    // allows multiple users but single provider!
+    template <class Key>
+    static void _connect(Assembly<>& assembly, _PortAddress<Key> user, _PortAddress<Key> provider) {
+        MPIAssembly& a = dynamic_cast<MPIAssembly&>(assembly);
+        string nuser = user.address.key.get(), nprov = provider.address.key.get();
+        string puser = user.prop, pprov = provider.prop;
+        int rprov = a.resource(nprov)[0];
+        if (a.local(nprov)) {
+            for (auto p : a.resource(nuser)) {
+                a.at(nprov).set(pprov, MPIPort(p, 17));
+            }
+        }
+        if (a.local(nuser)) {
+            a.at(nuser).set(puser, MPIPort(rprov, 17));
+        }
+    }
+};
+
+/*
+===================================================================================
+  User-defined classes
+===================================================================================*/
 class MPIReducer : public Component, public MPIConfig {
     vector<MPIPort> ports;
     void addPort(MPIPort port) { ports.push_back(port); }
@@ -86,63 +143,22 @@ class LocalSender : public Component, public MPIConfig {
     }
 };
 
-class MPIModel : public Model<>, public MPIConfig {
-    map<string, bool> resources;
-
-  public:
-    void resource(const string& compo, const set<int>& s) { resources[compo] = (s.find(rank) != s.end()); }
-    bool local(const string& compo) {
-        try {
-            return resources.at(compo);
-        } catch (std::out_of_range) {
-            return false;
-        }
-    }
-};
-
-Model<> emptymodel;
-
-class MPIAssembly : public Assembly<>, public MPIConfig {
-  public:
-    explicit MPIAssembly(MPIModel& model) : Assembly<>(emptymodel) {
-        for (auto& c : model.components) {
-            if (model.local(c.first)) {
-                instances.emplace(c.first, std::unique_ptr<Component>(c.second._constructor()));
-            }
-        }
-        for (auto& o : model.operations) {
-            o._connect(*this);
-        }
-    }
-};
-
-set<int> interval(int start, int end) {
-    int aend = (end == -1) ? MPIConfig().size : end;
-    set<int> result;
-    for (int i = start; i < aend; i++) {
-        result.insert(i);
-    }
-    return result;
-}
-
+/*
+===================================================================================
+  Main
+===================================================================================*/
 int main() {
     MPI_Init(NULL, NULL);
-    int rank = MPIConfig().rank, size = MPIConfig().size;
+    int rank = MPIConfig().rank;
 
     MPIModel model;
     model.component<MPIReducer>("reducer");
-    model.resource("reducer", set<int>{0});
+    model.resource("reducer", vector<int>{0});
 
     model.component<LocalSender>("sender", (rank * 11 + 3) % 7);
     model.resource("sender", interval(1, -1));
 
-    if (rank == 0) {
-        for (int i = 0; i < size - 1; i++) {
-            model.connect<Set>(PortAddress("ports", "reducer"), MPIPort{i + 1, i + 1});
-        }
-    } else {
-        model.connect<Set>(PortAddress("port", "sender"), MPIPort{0, rank});
-    }
+    model.connect<MPIp2p>(PortAddress("port", "sender"), PortAddress("ports", "reducer"));
 
     MPIAssembly assembly(model);
     if (rank == 0) {
